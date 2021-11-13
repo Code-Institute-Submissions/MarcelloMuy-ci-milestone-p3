7a1a01c57bbf0e20016c831d7b1242dd94b5f9d0
MarcelloMuy <marcellomuylaert@icloud.com>
2021-11-13 14:56:00 +0000

    Create choose_meals function to get user choice of dish

--- run.py
+++ run.py
@@ -15,10 +15,27 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('easy_groceries')
 
 
+def choose_meals():
+    """
+    Print dish type options to the user and get user option
+    """
+    print('Please select a dish type:')
+    print('Type 1 for Vegetarian')
+    print('Type 2 for Chicken')
+    print('Type 3 for Beef')
+    print('Type 4 for Fish')
+
+    user_option = input('Enter your option here: ')
+    print(f'You typed: {user_option}')
+
+
 def get_dish_type(dish_type):
     """
     Get list of recipes of specific type
     """
     meals_list = SHEET.worksheet(dish_type)
     data = meals_list.get_all_values()
-    print(data)
+    return data
+
+
+choose_meals()
