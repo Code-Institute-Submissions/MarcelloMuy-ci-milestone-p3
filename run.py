8af13d3e399281f7175dab9c681d86c661f164a9
MarcelloMuy <marcellomuylaert@icloud.com>
2021-11-14 12:48:34 +0000

    Create function to validade user dish choice data

--- run.py
+++ run.py
@@ -17,6 +17,8 @@ SHEET = GSPREAD_CLIENT.open('easy_groceries')
 def choose_meals():
     """
     Print dish type options to the user and get user choice.
+    Validate input data.
+    Call function to create a recipes dict and display it to the user.
     """
     while True:
         print('Please select a dish type:')
@@ -33,11 +35,17 @@ def choose_meals():
             print(f'You choose {str_user_choice}\n')
             break
 
-    recipes_dict = create_dict_recipes(str_user_choice)
-    print('Plese type recipe number to add it to groceries list.\n')
-    print(f'{recipes_dict}\n')
-    dish_choice = input('Enter your choice here: ')
-    print(dish_choice)
+    while True:
+        recipes_dict = create_dict_recipes(str_user_choice)
+        choose_meals.recipes_list = list(recipes_dict.keys())
+        print('Plese type recipe number to add it to groceries list.\n')
+        print(f'{recipes_dict}\n')
+
+        dish_choice = input('Enter your choice here: ')
+
+        if validate_dish_choice(dish_choice):
+            print(choose_meals.recipes_list[int(dish_choice) - 1])
+            break
 
 
 def validate_data(value):
@@ -48,7 +56,26 @@ def validate_data(value):
     try:
         if int(value) < 1 or int(value) > 4:
             raise ValueError(
-                f'Choose an option between 1 and 4, you choose {int(value)}'
+                f'Pick a number between 1 and 4, you choose {int(value)}'
+            )
+    except ValueError as e:
+        print(f'Invalid data: {e}, please try again.\n')
+        return False
+
+    return True
+
+
+def validate_dish_choice(value):
+    """
+    From inside the try converts value to integer.
+    Raises ValueError if value is not an integer
+    between 1 and and recipes list length.
+    """
+    list_len = len(choose_meals.recipes_list)
+    try:
+        if int(value) < 0 or int(value) >= list_len + 1:
+            raise ValueError(
+                f'Pick a number between 1 and {list_len}, you choose {value}'
             )
     except ValueError as e:
         print(f'Invalid data: {e}, please try again.\n')
@@ -59,7 +86,7 @@ def validate_data(value):
 
 def get_dish_type(dish_type):
     """
-    Convert int into str.
+    Convert integer to string.
     """
     if dish_type == 1:
         dish_type = 'vegetarian_recipes'
